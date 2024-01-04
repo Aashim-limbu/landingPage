c3e6605c721ee1f23e13bb1653de5d5f0ae792c9
Aashim <thelastmanarmy37@gmail.com>
2024-01-04 15:58:24 +0545

    added the scroll

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,32 @@
+import { animateScroll as scroll } from "react-scroll";
 import Hero from "./components/Hero";
 import NavBar from "./components/NavBar";
-import "./App.css"
+import "./App.css";
 import Companies from "./components/Companies";
 import Courses from "./components/Courses";
 import Acheivement from "./components/Acheivement";
 import Categories from "./components/Categories";
 import FeedBack from "./components/FeedBack";
 import Qta from "./components/Qta";
+import { useEffect } from "react";
 function App() {
+	useEffect(() => {
+		function scrollToBottom() {
+			scroll.scrollToBottom();
+		}
+		scrollToBottom();
+	}, []);
 	return (
-        <div className="relative">
-            <NavBar/>
-            <Hero/>
-            <Companies/>
-            <Courses/>
-            <Acheivement/>
-            <Categories/>
-            <FeedBack/>
-            <Qta/>
-        </div>
+		<div className="relative">
+			<NavBar />
+			<Hero />
+			<Companies />
+			<Courses />
+			<Acheivement />
+			<Categories />
+			<FeedBack />
+			<Qta />
+		</div>
 	);
 }
 
